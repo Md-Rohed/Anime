649b6372cb4d23b6fadc573de97c7bc23ee793a9
rohed-appnap <rohed@appnap.io>
2024-04-27 20:02:09 +0600

    api integrated for screenshot and external link

--- app/animes/[id]/page.js
+++ app/animes/[id]/page.js
@@ -1,17 +1,34 @@
 import {
-  fetchAnimeDetails, fetchSimilarAnimies,
+  fetchAnimeDetails, fetchAnimieExternalLinks, fetchAnimieScreenshots, fetchSimilarAnimies,
 } from "@/app/action";
 import AnimeCard from "@/components/animeCard";
 import Image from "next/image";
+import Link from "next/link";
 
 async function Animes({ params }) {
   const data = await fetchAnimeDetails(params.id);
   const similarData = await fetchSimilarAnimies(params.id);
+  const externalLinks = await fetchAnimieExternalLinks(params.id);
+  const screenShots = await fetchAnimieScreenshots(params.id);
+
+
 
   return (
     <div className="pt-[3rem] px-[1.25rem] md:px-[3.25rem] lg:px-[7.25rem]">
       <div className="flex flex-col md:flex-row justify-between gap-[2rem] pb-[2rem] ">
         <div className="flex gap-2">
+        <div className="flex flex-col mt-[1rem] gap-2 flex-wrap">
+            {screenShots.slice(0, 8).map((imageData, index) => (
+              <Image
+                src={`https://shikimori.one${imageData.original}`}
+                alt={data.name}
+                height={100}
+                width={100}
+                className=""
+                key={index}
+              />
+            ))}
+          </div>
           <div className="w-full">
             <Image
               src={`https://shikimori.one${data.image.original}`}
@@ -26,6 +43,18 @@ async function Animes({ params }) {
         <div className="w-full md:w-[70%]">
           <h1 className="font-bol text-[3rem]">{data.name}</h1>
           <p className="pt-[2rem]">{data.description}</p>
+          <div className="flex mt-[1rem] gap-2 flex-wrap ">
+            {externalLinks.slice(0, 8).map((externalData, index) => (
+              <Link
+                className="bg-black text-white p-3 hover:bg-gray-800"
+                target="_blank"
+                href={externalData.url}
+                key={index}
+              >
+                {externalData.kind}
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
 
